ee6465a9bf8b5a16645c79fb4e224d7a63f8dca0
Patrick OCallaghan <patrickocal@gmail.com>
2022-08-12 13:48:43 +1000

    added sector T to anzsic, see comb-workbook-for-kapital-flows.xlsx for sandbox for dealing with capital flows and difficulties razing

--- code/capitalFlows.jl
+++ code/capitalFlows.jl
@@ -1,8 +1,12 @@
+#import Pkg; Pkg.add("Tables", "XLSX", "ExcelReaders", "DataFrames",
+#       "JuMP", "Ipopt", "NamedArrays")
+
 include("concordance.jl")
+
 
 using DataFrames, JuMP, Ipopt, DelimitedFiles;
 
-#Change GFCF_By_Industry_Asset to 19 sector
+#Change GFCF_By_Industry_Asset to 20 sector
 flows97 = ExcelReaders.readxlsheet("data"*pathmark*"flow1997.xls", "180x22Combined");
 flows = DataFrame(flows97[4:182, 4:25], :auto);
 dataTypeEx = flows[41,4]
@@ -14,16 +18,17 @@ for i in [1:1:179;]
     end
 end
 
-# Aggregating to 19 sector
+# Aggregating to 20 sector
 flows.x5 = flows.x5+flows.x6+flows.x7;
 flows = select!(flows, Not(:x6));
 flows = select!(flows, Not(:x7));
-flows.x10 = flows.x10+flows.x11;
+flows.x10 = flows.x10 + flows.x11;
 flows = select!(flows, Not(:x11));
-flows.x15 = flows.x15+flows.x16;
+flows.x15 = flows.x15 + flows.x16;
 flows = select!(flows, Not(:x16));
-
-# Renaming to 19 Sectors codes
+n = ncol(flows) + 1;
+insertcols!(flows, n, :T => zeros(length(flows.x1)));
+# Renaming to 20 Sectors codes
 rename!(flows, :x1 => :A)
 rename!(flows, :x2 => :B)
 rename!(flows, :x3 => :D)
@@ -46,103 +51,169 @@ rename!(flows, :x22 => :S)
 rowCode = flows97[4:182,2];
 containsSpace = findall( x -> occursin(" ", x), rowCode);
 rowCode[containsSpace] = replace.(rowCode[containsSpace], " " => "");
-rowCode19 = Array{Union{Nothing, String}}(nothing, length(rowCode));
+rowCode20 = Array{Union{Nothing, String}}(nothing, length(rowCode));
 for i in eachindex(rowCode);
-    rowCode19[i] = Comm180To19[rowCode[i]];
+    rowCode20[i] = Comm180To20[rowCode[i]];
 end
-insertcols!(flows ,1, :Industry => rowCode19);
+insertcols!(flows ,1, :Industry => rowCode20);
 insertcols!(flows ,15, :O => zeros(179));
 splitIndustry = groupby(flows, :Industry);
 flows = combine(splitIndustry, valuecols(splitIndustry) .=> sum);
 
-push!(flows, ["A" zeros(1,19)])
-push!(flows, ["D" zeros(1,19)])
-push!(flows, ["H" zeros(1,19)])
-push!(flows, ["K" zeros(1,19)])
-push!(flows, ["N" zeros(1,19)])
-push!(flows, ["P" zeros(1,19)])
-push!(flows, ["Q" zeros(1,19)])
-push!(flows, ["R" zeros(1,19)])
-push!(flows, ["S" zeros(1,19)])
+push!(flows, ["A" zeros(1, ncol(flows) - 1)])
+push!(flows, ["D" zeros(1, ncol(flows) - 1)])
+push!(flows, ["H" zeros(1, ncol(flows) - 1)])
+push!(flows, ["K" zeros(1, ncol(flows) - 1)])
+push!(flows, ["N" zeros(1, ncol(flows) - 1)])
+push!(flows, ["P" zeros(1, ncol(flows) - 1)])
+push!(flows, ["Q" zeros(1, ncol(flows) - 1)])
+push!(flows, ["R" zeros(1, ncol(flows) - 1)])
+push!(flows, ["S" zeros(1, ncol(flows) - 1)])
 
-
-
-sort!(flows)
+#sort!(flows)
 
 #flows = select!(flows, Not(:Industry));
 
-#insertcols!(flows ,20, :Dwelling => zeros(19));
+#insertcols!(flows ,20, :Dwelling => zeros(20));
 #push!(flows, zeros(1,20));
 
 # print(flows)
 
 
-# Make the big RAS (zero prior)
-
 # Bring in GFCF data from excel
-ausGFCF = ExcelReaders.readxlsheet("data"*pathmark*"5204064_GFCF_By_Industry_Asset.xls", "Data1");
-ausGFCF = ausGFCF[:,Not(findall(x -> occursin("ALL INDUSTRIES ;", x), string.(ausGFCF[1,:])))]
-# Select only totals
-ausGFCF2019 = ausGFCF[findall(x -> occursin("2019", x), string.(ausGFCF[:,1])),
-    findall(x -> occursin("Gross fixed capital formation: Current prices ;", x), string.(ausGFCF[1,:]))];
-# Add dwelling data to the end
-ausGFCF2019=[vec(ausGFCF2019); (ausGFCF[findall(x -> occursin("2019", x), string.(ausGFCF[:,1])),
-findall(x -> occursin("Dwellings: Current prices ;", x), string.(ausGFCF[1,:]))]
-+ausGFCF[findall(x -> occursin("2019", x), string.(ausGFCF[:,1])),
-findall(x -> occursin("Ownership transfer costs: Current prices ;", x), string.(ausGFCF[1,:]))])];
-
+ausGFCFall = ExcelReaders.readxlsheet(
+  "data"*pathmark*"5204064_GFCF_By_Industry_Asset.xls", "Data1"
+);
+ausGFCFall[1, 1] = "top corner";
+# remove columns
+ausGFCFall = ausGFCFall[:,
+  Not(findall(x -> occursin("ALL INDUSTRIES ;", x), string.(ausGFCFall[1,:])))
+];
+# select current totals
+ausGFCFcurrent = ausGFCFall[:,
+  findall(x -> 
+          occursin("Gross fixed capital formation: Current", x)
+          | occursin("Dwellings: Current", x)
+          | occursin("transfer costs: Current", x),
+    string.(ausGFCFall[1,:]))
+];
+# Select dates column
+ausGFCFdates = ausGFCFall[:,1];
+ausGFCFcurrent = hcat(ausGFCFdates, ausGFCFcurrent);
+ausGFCFrow= ausGFCFcurrent[
+  findall(x -> occursin("2007", x), string.(ausGFCFcurrent[:,1])), :];
+#ausGFCFrow[length(ausGFCFrow) - 1] = (ausGFCFrow[length(ausGFCFrow) - 1]
+#                                      + ausGFCFrow[length(ausGFCFrow)]);
+ausGFCFrow = ausGFCFrow[Not(1, length(ausGFCFrow))];
 # Make a new Dict
-IOIGAs19=Array{Union{Nothing, String}}(nothing, length(IOIG));
+IOIGAs20=Array{Union{Nothing, String}}(nothing, length(IOIG));
 for i in eachindex(IOIG);
-    IOIGAs19[i] = IOIGTo19[IOIG[i]]
+    IOIGAs20[i] = IOIGTo20[IOIG[i]]
 end
 
+#==============================================================================
+wrangle the IO table
+==============================================================================#
+# what year are we importing?
+fyend = "2007"
 # Import IO data
-IOSource8 = ExcelReaders.readxlsheet("data"*pathmark*"5209055001DO001_201819.xls", "Table 8");
-# Isolate GFCF
-privateGFCF = IOSource8[4:117,findall(x -> occursin("Private ; Gross Fixed Capital Formation", x), string.(IOSource8[2,:]))]
-publicGFCF = IOSource8[4:117,findall(x -> occursin("Public Corporations ; Gross Fixed Capital Formation", x), string.(IOSource8[2,:]))]
-govGFCF = IOSource8[4:117,findall(x -> occursin("General Government ; Gross Fixed Capital Formation", x), string.(IOSource8[2,:]))]
-
-# Grouping GFCF receivable by 19 sectors (dwellings is all zero in capital)
-ausCapReceiv = DataFrame(privateGFCF+publicGFCF+govGFCF, [:GFCF]);
-insertcols!(ausCapReceiv ,1, :Industry => IOIGAs19);
-splitIndustry = groupby(ausCapReceiv, :Industry);
-ausCapReceiv = combine(splitIndustry, valuecols(splitIndustry) .=> sum);
-sort!(ausCapReceiv);
-
-# Add final 0 on for ownership of dwellings
-ausCapReceivable = [ausCapReceiv.GFCF_sum; 0];
-
-# Balancing sums, there is only a small difference but IO data is king so we scale to that
-ausCapReceivableSum = sum(ausCapReceivable)
-ausGFCF2019Sum = sum(ausGFCF2019)
-for i in eachindex(ausGFCF2019)
-    ausGFCF2019[i]=ausGFCF2019[i]*ausCapReceivableSum/ausGFCF2019Sum;
+iotable8 = DataFrame(CSV.File("data"*pathmark*fyend*pathmark*"table8.csv"));
+iotable5 = DataFrame(CSV.File("data"*pathmark*fyend*pathmark*"table5.csv"));
+# instantiate a variable for the number of sectors
+numsec = 0
+occursin("111", string(iotable8[5, 1])) ? numsec = 111 : numsec = 114
+# since numsec is an array
+numsec = numsec[1];
+# find the title row index
+titlerow = findall(x -> occursin("USE", x), string.(iotable8[:,2]));
+# since titlerow is of type array and we only need its value
+titlerow = titlerow[1]
+# check the two tables have the same year
+commonwealthcell = titlerow + 2 + numsec + 17
+(iotable8[commonwealthcell, 2] != iotable5[commonwealthcell, 2]
+ ? println("WARNING: io table release years don't match!")
+ : println("io table release years match"))
+# standardise the table
+numcol = numsec + 12;
+iotable8 = iotable8[:, 1:numcol];
+# create column titles
+coltitles = collect(values(iotable8[titlerow, :]))
+coltitles[2] = "industry"
+morecoltitles = collect(values(iotable8[titlerow + 2,
+                                        numsec + 3 : numsec + 12]))
+coltitles[numsec + 3 : numsec + 12] = morecoltitles
+coltitles = filter.(x -> !isspace(x), coltitles)
+coltitles[1] = "IOIG"
+for i in [1:1:numsec;]
+  coltitles[i + 2] = "IOIG"*values(coltitles[i + 2])
 end
+rename!(iotable8, string.(coltitles), makeunique=true)
+# remove initial rows
+iotable8 = iotable8[Not(range(1, titlerow + 2)), :]
+iotable8 = dropmissing(iotable8, :industry)
+iotable8 = iotable8[Not(findall(x -> occursin("Commonwealth", x),
+                                string.(iotable8.industry))),:]
+# convert to numbers
+iotable8[:, 3:numcol] = filter.(x -> !isspace(x), iotable8[:, 3:numcol])
+# Isolate GFCF
+Q3 = parse.(Float64, iotable8.Q3)
+Q4 = parse.(Float64, iotable8.Q4)
+Q5 = parse.(Float64, iotable8.Q5)
+
+ioigGfcf = DataFrame(:inv => Q3 + Q4 + Q5);
+# Grouping GFCF receivable by 20 sectors (dwellings is all zero in capital)
+break
+insertcols!(ioigGfcf, 1, :Industry => IOIGAs20);
+splitIndustry = groupby(ioigGfcf, :Industry);
+anzdivgfcf = combine(splitIndustry, valuecols(splitIndustry) .=> sum);
+sort!(anzdivgfcf);
+
+# Balancing row and column sums to IO table 8 total
+ausprodrow = findfirst(x -> occursin("Australian Production", x),
+                  string.(iotable8.industry))
+anzdivgfcfsum = anzdivgfcf.inv_sum[ausprodrow]
+break
+ausGFCFsum = sum(ausGFCFrow);
+for i in eachindex(ausGFCFrow)
+    ausGFCFrow[i] = ausGFCFrow[i] * anzdivgfcfsum / ausGFCFsum;
+end;
+# pull in proportionalised kapital flows to ras
+#y = DataFrame(CSV.File("data/propd-to-ras.csv"))
+#y = Matrix(y)
+# generate an initial table for the ras
+y = zeros(length(ausGFCFrow), length(anzdivgfcf.inv_sum))
+for i in eachindex(ausGFCFrow), j in eachindex(anzdivgfcf);
+  y[i, j] = anzdivgfcf[i] / anzdivgfcfsum * ausGFCFrow[j]
+end  
 
 # Begin Optimisation
 modCap = Model(Ipopt.Optimizer);
 # Should be equal dimensions
-@variable(modCap, x[1:length(ausCapReceivable), 1:length(ausGFCF2019)] >=0);
-# Lowest entropy objective
-@NLobjective(modCap, Min, sum((x[i,j] - 0)^2 for i in eachindex(ausCapReceivable), j in eachindex(ausGFCF2019)));
+@variable(modCap, x[1:length(anzdivgfcf.inv_sum),
+                    1:length(ausGFCFrow)] >= 0);
+# Max entropy objective (or min relative to uniform)
+@NLobjective(modCap,
+             Min,
+             sum((x[i, j] - y[i, j]) ^ 2
+                 for i in eachindex(anzdivgfcf.inv_sum),
+                 j in eachindex(ausGFCFrow)
+                )
+            );
+
 # Row-sums constraint - must be equal to the GFCF totals
-for j in eachindex(ausGFCF2019)
-    @constraint(modCap, sum(x[:,j]) == ausGFCF2019[j]);
+for j in eachindex(ausGFCFrow)
+    @constraint(modCap, sum(x[:,j]) == ausGFCFrow[j]);
 end;
 # Col-sums constraint - must be equal to the IO totals
-for k in eachindex(ausCapReceivable)
-    @constraint(modCap, sum(x[k,:]) == ausCapReceivable[k]);
+for k in eachindex(anzdivgfcf.inv_sum)
+    @constraint(modCap, sum(x[k,:]) == anzdivgfcf.inv_sum[k]);
 end;
 optimize!(modCap);
 
 # Add titles etc. for ease of reading -J works but this is clumsy fix 
-capitalFlowsTitleRow = permutedims([ANZSICDivShort; "Dwelling"]);
-capitalFlowsTitleCol = ["Titles"; ANZSICDivShort; "Dwelling"];
-capitalFlowsRAS =[capitalFlowsTitleCol [capitalFlowsTitleRow; value.(x)]]
-capitalFlowsRAS = DataFrame(capitalFlowsRAS[2:size(capitalFlowsRAS, 1), :], capitalFlowsRAS[1,:])
+ySol2 = DataFrame(value.(x), ANZSICDivShort);
+insertcols!(ySol2, 1, :Divisions => ANZSICDivShort);
 
 # Export tables as CSV
-CSV.write("data"*pathmark*"capitalFlowsRAS.csv", capitalFlowsRAS);
+CSV.write("data"*pathmark*"capitalFlowsRAS2.csv", ySol2);
 CSV.write("data"*pathmark*"UScapitalFlows.csv", flows);
--- code/concordance.jl
+++ code/concordance.jl
@@ -1,4 +1,5 @@
-#concordance dict between ANZSIC divisions (19 sectors) and various other industry classifications
+#concordance dict between ANZSIC divisions (19 sectors plus Ownership of
+#Dwellings) and various other industry classifications
 using XLSX, ExcelReaders, DataFrames, Tables, JuMP, Ipopt, NamedArrays, DelimitedFiles, CSV, Tables;
 
 #filepath cross system compatability code
@@ -8,100 +9,125 @@ else
     pathmark = "/";
 end
 
-#19 Sector to 4 Sector
-from19To4 = Dict("A"=> "Primary",
-"B" => "Primary",
-"C" => "Secondary",
-"D" => "Secondary",
-"E" => "Secondary",
-"F" => "Tertiary",
-"G" => "Tertiary",
-"H" => "Tertiary",
-"I" => "Tertiary",
-"J" => "Tertiary",
-"K" => "Tertiary",
-"L" => "Tertiary",
-"M" => "Tertiary",
-"N" => "Tertiary",
-"O" => "Tertiary",
-"P" => "Tertiary",
-"Q" => "Tertiary",
-"R" => "Tertiary",
-"S" => "Tertiary")
+#20 Sector to 4 Sector
+from20To4 = Dict(
+  "A"=> "Primary",
+  "B" => "Primary",
+  "C" => "Secondary",
+  "D" => "Secondary",
+  "E" => "Secondary",
+  "F" => "Tertiary",
+  "G" => "Tertiary",
+  "H" => "Tertiary",
+  "I" => "Tertiary",
+  "J" => "Tertiary",
+  "K" => "Tertiary",
+  "L" => "Tertiary",
+  "M" => "Tertiary",
+  "N" => "Tertiary",
+  "O" => "Tertiary",
+  "P" => "Tertiary",
+  "Q" => "Tertiary",
+  "R" => "Tertiary",
+  "S" => "Tertiary",
+  "T" => "Ownership"
+ )
 
-#IOIG to 19 Sector
-IOSource = ExcelReaders.readxlsheet("data"*pathmark*"5209055001DO001_201819.xls", "Table 5");
+#IOIG to 20 Sector
+IOSource = ExcelReaders.readxlsheet("data"*pathmark*"5209055001DO001_201819.xls", "Table 8");
 IOIG = IOSource[4:117, 1];
-ANZSICDiv = ["Agriculture, forestry and fishing", "Mining", "Manufacturing", "Electricity, gas, water and waste services", 
-"Construction", "Wholesale trade", "Retail trade", "Accomodation and food services", 
-"Transport, postal and warehousing", "Information media and telecommunications", "Financial and insurance services", 
-"Rental, hiring and real estate services", "Professional, scientific and technical services", 
-"Administrative and support services", "Public administration and safety", "Education and training", 
-"Health care and social assistance", "Arts and recreation services", "Other services"];
-ANZSICDivShort = ["AgrForestFish", "Mining", "Manufacturing", "Utilities", "Construction", "Wholesale", "Retail", "AccomFoodServ", 
-"Transport&Ware", "Communications", "Finance&Insur", "RealEstate", "BusinessServ", "Admin", "PublicAdminSafe", 
-"Education", "Health&Social", "Arts&Rec", "OtherServices"];
-ANZSICDivByLetter =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S"];
-from19ToInd = Dict{String, Int64}();
+ANZSICDiv = ["Agriculture,
+             forestry and fishing",
+             "Mining",
+             "Manufacturing",
+             "Electricity, gas, water and waste services",
+             "Construction",
+             "Wholesale trade",
+             "Retail trade",
+             "Accomodation and food services",
+             "Transport, postal and warehousing",
+             "Information media and telecommunications",
+             "Financial and insurance services",
+             "Rental, hiring and real estate services",
+             "Professional, scientific and technical services",
+             "Administrative and support services",
+             "Public administration and safety",
+             "Education and training",
+             "Health care and social assistance",
+             "Arts and recreation services",
+             "Other services",
+             "Ownership of Dwellings",
+            ];
+ANZSICDivShort = ["AgrForestFish", "Mining", "Manufacturing", "Utilities",
+                  "Construction", "Wholesale", "Retail", "AccomFoodServ",
+                  "Transport&Ware", "Communications", "Finance&Insur",
+                  "RealEstate", "BusinessServ", "Admin", "PublicAdminSafe",
+                  "Education", "Health&Social", "Arts&Rec", "OtherServices",
+                  "Dwellings"];
+ANZSICDivByLetter =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
+                    "P","Q","R","S", "T"];
+from20ToInd = Dict{String, Int64}();
 for i in eachindex(ANZSICDivByLetter);
-    from19ToInd[ANZSICDivByLetter[i]] = Int(i);
+    from20ToInd[ANZSICDivByLetter[i]] = Int(i);
 end
-IOIGTo19 = Dict{Float64, String}();
-for i in [1:1:114;];
-    test = trunc(IOIG[i]/100);
-    if 1 <= test <= 5
-        IOIGTo19[IOIG[i]]="A"
-    elseif 6 <= test <= 10;
-        IOIGTo19[IOIG[i]]="B"
-    elseif 11 <= test <= 25;
-        IOIGTo19[IOIG[i]]="C"
-    elseif 26 <= test <= 29;
-        IOIGTo19[IOIG[i]]="D"
-    elseif 30 <= test <= 32;
-        IOIGTo19[IOIG[i]]="E"
-    elseif 33 <= test <= 38;
-        IOIGTo19[IOIG[i]]="F"
-    elseif 39 <= test <= 43;
-        IOIGTo19[IOIG[i]]="G"
-    elseif 44 <= test <= 45;
-        IOIGTo19[IOIG[i]]="H"
-    elseif 46 <= test <= 53;
-        IOIGTo19[IOIG[i]]="I"
-    elseif 54 <= test <= 60;
-        IOIGTo19[IOIG[i]]="J"
-    elseif 62 <= test <= 64;
-        IOIGTo19[IOIG[i]]="K"
-    elseif 66 <= test <= 67;
-        IOIGTo19[IOIG[i]]="L"
-    elseif 69 <= test <= 70;
-        IOIGTo19[IOIG[i]]="M"
-    elseif 72 <= test <= 73;
-        IOIGTo19[IOIG[i]]="N"
-    elseif 75 <= test <= 77;
-        IOIGTo19[IOIG[i]]="O"
-    elseif 80 <= test <= 82;
-        IOIGTo19[IOIG[i]]="P"
-    elseif 84 <= test <= 87;
-        IOIGTo19[IOIG[i]]="Q"
-    elseif 89 <= test <= 92;
-        IOIGTo19[IOIG[i]]="R"
-    elseif 94 <= test <= 96;
-        IOIGTo19[IOIG[i]]="S"
+IOIGTo20 = Dict{Float64, String}();
+for i in [1:1:114;]
+  test = IOIG[i] / 100
+    if 1 <= test < 6
+        IOIGTo20[IOIG[i]]="A"
+    elseif 6 <= test < 10
+        IOIGTo20[IOIG[i]]="B"
+    elseif 10 <= test < 26
+        IOIGTo20[IOIG[i]]="C"
+    elseif 26 <= test < 30
+        IOIGTo20[IOIG[i]]="D"
+    elseif 30 <= test < 33
+        IOIGTo20[IOIG[i]]="E"
+    elseif 33 <= test < 39
+        IOIGTo20[IOIG[i]]="F"
+    elseif 39 <= test < 44
+        IOIGTo20[IOIG[i]]="G"
+    elseif 44 <= test < 46
+        IOIGTo20[IOIG[i]]="H"
+    elseif 46 <= test < 54
+        IOIGTo20[IOIG[i]]="I"
+    elseif 54 <= test < 62
+        IOIGTo20[IOIG[i]]="J"
+    elseif 62 <= test < 66
+        IOIGTo20[IOIG[i]]="K"
+      elseif (66 <= test < 67 || 67.02 <= test < 69) #ownership of dwellings
+        IOIGTo20[IOIG[i]]="L"
+    elseif 69 <= test < 72
+        IOIGTo20[IOIG[i]]="M"
+    elseif 72 <= test < 75
+        IOIGTo20[IOIG[i]]="N"
+    elseif 75 <= test < 80
+        IOIGTo20[IOIG[i]]="O"
+    elseif 80 <= test < 84
+        IOIGTo20[IOIG[i]]="P"
+    elseif 84 <= test < 89
+        IOIGTo20[IOIG[i]]="Q"
+    elseif 89 <= test < 94
+        IOIGTo20[IOIG[i]]="R"
+    elseif 94 <= test < 96
+        IOIGTo20[IOIG[i]]="S"
+    elseif 67 <= test < 67.02
+        IOIGTo20[IOIG[i]]="T"
     else
         print("ERROR: An input has fallen outside of the range of categories")
     end
 end
 
-#ISIC 4.0 To 19 Sectors
+#ISIC 4.0 To 20 Sectors
 ANZSICISICSource = CSV.read("data"*pathmark*"ANZSIC06-ISIC3pt1.csv", DataFrame);
-ANZSIC19 = ANZSICISICSource[6:1484, 1][findall(x -> typeof(x)<:String15, ANZSICISICSource[6:1484, 4])];
+ANZSIC20 = ANZSICISICSource[6:1484, 1][findall(x -> typeof(x)<:String15, ANZSICISICSource[6:1484, 4])];
 ISIC = ANZSICISICSource[6:1484, 4][findall(x -> typeof(x)<:String15, ANZSICISICSource[6:1484, 4])];
 for i in eachindex(ISIC);
     ISIC[i]=strip(ISIC[i], ['p']);
 end
-ISICTo19 = Dict(ISIC .=> ANZSIC19);
+ISICTo20 = Dict(ISIC .=> ANZSIC20);
 
-#NAIC2007 To 19 Sectors via ISIC 4.0
+#NAIC2007 To 20 Sectors via ISIC 4.0
 NAICSISICSource = ExcelReaders.readxlsheet("data"*pathmark*"2007_NAICS_to_ISIC_4.xls", "NAICS 07 to ISIC 4 technical");
 NAICS = string.(Int.(NAICSISICSource[4:1768,1]));
 ISICAsANZSIC = NAICSISICSource[4:1768,3];
@@ -109,35 +135,35 @@ ISICAsANZSIC = string.(ISICAsANZSIC);
 containsX = findall( x -> occursin("X", x), ISICAsANZSIC);
 ISICAsANZSIC[containsX] = replace.(ISICAsANZSIC[containsX], "X" => "1");
 ISICAsANZSIC = parse.(Float64, ISICAsANZSIC);
-NAICSANZSIC19 = string.(zeros(length(ISICAsANZSIC)));
+NAICSANZSIC20 = string.(zeros(length(ISICAsANZSIC)));
 for i in eachindex(ISICAsANZSIC);
-    NAICSANZSIC19[i] = ISICTo19[lpad(Int(ISICAsANZSIC[i]),4,"0")];
+    NAICSANZSIC20[i] = ISICTo20[lpad(Int(ISICAsANZSIC[i]),4,"0")];
 end
-NAICS07To19 = Dict(NAICS .=> NAICSANZSIC19);
+NAICS07To20 = Dict(NAICS .=> NAICSANZSIC20);
 
-#NAIC2002 To 19 Sectors via NAIC2007
+#NAIC2002 To 20 Sectors via NAIC2007
 NAICS02To07 = CSV.read("data"*pathmark*"2002_to_2007_NAICS.csv", DataFrame);
 NAICS02To0702 = string.(NAICS02To07[3:1202, 1]);
 NAICS02To0707 = string.(NAICS02To07[3:1202, 3]);
-NAICS07As19 = string.(zeros(length(NAICS02To0707)));
+NAICS07As20 = string.(zeros(length(NAICS02To0707)));
 for i in eachindex(NAICS02To0707);
-    NAICS07As19[i] = NAICS07To19[NAICS02To0707[i]];
+    NAICS07As20[i] = NAICS07To20[NAICS02To0707[i]];
 end
-NAICS02To19 = Dict(NAICS02To0702 .=> NAICS07As19);
+NAICS02To20 = Dict(NAICS02To0702 .=> NAICS07As20);
 
-#NAIC1997 To 19 Sectors via NAIC2002
+#NAIC1997 To 20 Sectors via NAIC2002
 NAICS97To02 = CSV.read("data"*pathmark*"1997_NAICS_to_2002_NAICS.csv", DataFrame);
 NAICS97To0297 = string.(NAICS97To02[1:1355, 1]);
 NAICS97To0202 = string.(NAICS97To02[1:1355, 3]);
-NAICS02As19 = string.(zeros(length(NAICS97To0202)));
+NAICS02As20 = string.(zeros(length(NAICS97To0202)));
 for i in eachindex(NAICS97To0202);
-    NAICS02As19[i] = NAICS02To19[NAICS97To0202[i]];
+    NAICS02As20[i] = NAICS02To20[NAICS97To0202[i]];
 end
-NAICS97To19 = Dict(NAICS97To0297 .=> NAICS02As19);
+NAICS97To20 = Dict(NAICS97To0297 .=> NAICS02As20);
 NAICS97To0297Trunc = first.(string.(NAICS97To02[1:1355, 1]),4);
-NAICS97To19Trunc = Dict(NAICS97To0297Trunc .=> NAICS02As19);
+NAICS97To20Trunc = Dict(NAICS97To0297Trunc .=> NAICS02As20);
 
-#Comm180 To 19 Sectors via NAIC 1997
+#Comm180 To 20 Sectors via NAIC 1997
 NAICS97ToComm180 = CSV.read("data"*pathmark*"NAICS_to_Comm180.csv", DataFrame);
 NAICS97ToComm18097 = first.([NAICS97ToComm180[1:90,4];NAICS97ToComm180[1:89,9]],4);
 containsStar = findall( x -> occursin("*", x), NAICS97ToComm18097);
@@ -149,17 +175,17 @@ containsStar = findall( x -> occursin("*", x), NAICS97ToComm180180);
 NAICS97ToComm180180[containsStar] = replace.(NAICS97ToComm180180[containsStar], "*" => "");
 containsSpace = findall( x -> occursin(" ", x), NAICS97ToComm180180);
 NAICS97ToComm180180[containsSpace] = replace.(NAICS97ToComm180180[containsSpace], " " => "");
-NAICS97As19 = string.(zeros(length(NAICS97ToComm18097)));
+NAICS97As20 = string.(zeros(length(NAICS97ToComm18097)));
 for i in eachindex(NAICS97ToComm18097);
     NAICS97ToComm18097[i] = rpad(parse(Int64, NAICS97ToComm18097[i]),4,"1");
 end
 Invalid4Dig = findall( x -> occursin("2311", x), NAICS97ToComm18097);
 NAICS97ToComm18097[Invalid4Dig].=["2331"];
 for i in eachindex(NAICS97ToComm18097);
-    NAICS97As19[i] = NAICS97To19Trunc[NAICS97ToComm18097[i]];
+    NAICS97As20[i] = NAICS97To20Trunc[NAICS97ToComm18097[i]];
 end
-Comm180To19=Dict(NAICS97ToComm180180 .=> NAICS97As19);
+Comm180To20=Dict(NAICS97ToComm180180 .=> NAICS97As20);
 
 #Final concordance
-finalConcordance = [NAICS97ToComm180180 NAICS97As19];
-writedlm("data"*pathmark*"Comm180To19Concordance.csv", finalConcordance, ',');
+finalConcordance = [NAICS97ToComm180180 NAICS97As20];
+writedlm("data"*pathmark*"Comm180To20Concordance.csv", finalConcordance, ',');
